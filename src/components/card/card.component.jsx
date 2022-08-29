a4bbcbb3107ad5a5fd2652306250b3bae9bb790b
Juan Moreno <juanignacio.moreno@globant.com>
2022-08-28 23:29:05 -0300

    fix(): change all the app to funcional component

--- src/components/card/card.component.jsx
+++ src/components/card/card.component.jsx
@@ -1,20 +1,14 @@
-import { Component } from "react"
-
 import "./card.styles.css"
 
-class Card extends Component {
-  render() {
-    const { id, name, email } = this.props.monster
-    const urlImage = "https://robohash.org/"
-    const urlKindImage = "?set=set2&size=341x341"
-    return (
-      <div className="card-container" key={id}>
-        <img src={`${urlImage}${id}${urlKindImage}`} alt={`monster ${name}`} />
-        <h2>{name}</h2>
-        <p>{email}</p>
-      </div>
-    )
-  }
-}
+const urlImage = "https://robohash.org/"
+const urlKindImage = "?set=set2&size=341x341"
+
+const Card = ({ monster: { id, name, email } }) => (
+  <div className="card-container" key={id}>
+    <img src={`${urlImage}${id}${urlKindImage}`} alt={`monster ${name}`} />
+    <h2>{name}</h2>
+    <p>{email}</p>
+  </div>
+)
 
 export default Card
--- src/components/search-box/search-box.component.jsx
+++ src/components/search-box/search-box.component.jsx
@@ -1,19 +1,12 @@
-import { Component } from "react"
-
 import "./search-box.styles.css"
 
-class SearchBox extends Component {
-  render() {
-    const { className, placeholder, onChangeHandler } = this.props
-    return (
-      <input
-        type="search"
-        className={className}
-        placeholder={placeholder}
-        onChange={onChangeHandler}
-      />
-    )
-  }
-}
+const SearchBox = ({ className, placeholder, onChangeHandler }) => (
+  <input
+    type="search"
+    className={className}
+    placeholder={placeholder}
+    onChange={onChangeHandler}
+  />
+)
 
 export default SearchBox
